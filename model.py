b8548fa1e9866132242ff4b74ada2e7480476897
Kundan-CR7 <Kundangupta7334@gmail.com>
2026-03-12 17:51:58 +0530

    implement ResidualConnection class

--- model.py
+++ model.py
@@ -110,3 +110,11 @@ class MultiHeadAttention(nn.Module):
         # (Batch,seq_len,d_model) --> (Batch,seq_len,d_model)
         return self.w_o(x)
 
+class ResidualConnection(nn.Module):
+    def __init__(self,dropout:float):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self,x,sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
